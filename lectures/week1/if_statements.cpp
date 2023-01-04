7e68b362302d38d278adf3820f6c1e31eb6784a1
Toby Donaldson <tjd@sfu.ca>
2023-01-03 19:36:28 -0800

    updated

---
+++ lectures/week1/if_statements.cpp
@@ -0,0 +1,22 @@
+// if_statements.cpp
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "What is the temperature in Celsius? ";
+    double temp = 0.0;
+    cin >> temp;
+    if (temp < 0.0) {
+        cout << "It is freezing!\n";
+    } else if (temp < 10.0) {
+        cout << "It is cold!\n";
+    } else if (temp < 20.0) {
+        cout << "It is cool!\n";
+    } else if (temp < 30.0) {
+        cout << "It is warm!\n";
+    } else {
+        cout << "It is hot!\n";
+    }
+}
