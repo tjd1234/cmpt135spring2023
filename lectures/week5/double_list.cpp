e59b62cebcb495ea291089f95c42e2108e9458dd
Toby Donaldson <tjd@sfu.ca>
2023-02-01 15:28:17 -0800

    updated

--- lectures/week5/double_list.cpp
+++ lectures/week5/double_list.cpp
@@ -1,4 +1,4 @@
-// double_list_oop.cpp
+// double_list.cpp
 
 #include <iostream>
 #include <cassert>
@@ -7,163 +7,176 @@
 
 using namespace std;
 
-struct double_list {
-// private members can only be accessed by could in double_list, and
-// cannot be accessed by code outside of double_list.
+class double_list {
 private:
     double* arr;    // pointer to the underlying array
     int capacity;   // length of underlying array
     int size;       // # of elements from user's perspective
-
-// public members can be accessed by any code.
 public:
-    // Default constructor: takes no input and makes an array of 
-    // size 0
+    // default constructor
     double_list()
-    : double_list(0)  // constructor delegation
+    : arr(new double[10]), capacity(10), size(0)  // initializer list
     { }
 
-    // double_list has a constructor that must be called whenever 
-    // you create a new double_list. It ensures all the variables 
-    // are properly initialized.
-    double_list(int n)
-    : capacity(2*n + 1), size(n)  // initializer list
+    // copy constructor
+    double_list(const double_list& lst)
+    : arr(new double[lst.capacity]), capacity(lst.capacity), size(lst.size)
     {
-        if (n < 0) 
-           cmpt::error("double_list(int n): n must be 0 or greater");
-        arr = new double[capacity];
-        for (int i = 0; i < size; i++) {
-            arr[i] = 0;
+        for(int i = 0; i < size; i++) {
+            arr[i] = lst.arr[i];
         }
     }
 
-    // Copy constructor: makes a copy of another double_list. The 
-    // copy has the same size, capacity and values, but its own
-    // underlying array.
-    double_list(const double_list& other) 
-    : arr(new double[other.capacity]), 
-      capacity(other.capacity), 
-      size(other.size)
-    {
-        for (int i = 0; i < size; i++) {
-            arr[i] = other.arr[i];
-        }
+    ~double_list() {
+        delete[] arr;
+        cout << "double_list destructor called\n";
     }
 
+    // getters
+    int get_capacity() const { return capacity; }
+    int get_size()     const { return size;     }
+    int get(int i) const {
+        assert(i >= 0 && i < size); // bounds checking
+        return arr[i];
+    }
+
+    // setters
+    void set(int i, double x) {
+        assert(i >= 0 && i < size); // bounds checking
+        arr[i] = x;
+    }
+
+    // prints lst to cout
+    void print() const {
+        for (int i = 0; i < size; i++) {
+            cout << "   arr[" << i << "] = " << arr[i] << "\n";
+        }
+        cout << "capacity = " << capacity << ", "
+             << "size = "     << size     << "\n\n";
+    }
+
+    // Appends the number x to the right end of the array, increasing it's size by
+    // 1. If necessary, it will also double the capacity.
+    //
+    // IMPORTANT: lst must be passed by reference since lst.size (and maybe also
+    // lst.capacity) is changed. 
+    //
+    // IMPORTANT: The line delete[] lst.arr fails if lst.capacity is 0. To prevent
+    // this, the make_double_empty_list must always sets the capacity to a value
+    // greater than 0.
     void append_right(double x) {
-        if (size >= capacity) {
-            // double the capacity of the array
-
-            // make a new array about twice the size of the current
-            // one
-            capacity = 2 * capacity;
-            double* arr_new = new double[capacity];
-
-            // copy the elements from the old array into the new one
-            for(int i = 0; i < size; i++) {
-                arr_new[i] = arr[i]; 
+        // double the capacity of array, if necessary
+        if (size >= capacity) { 
+            capacity = 2 * capacity;                // make a new array 
+            double* arr_new = new double[capacity]; // twice the size 
+            
+            for(int i = 0; i < size; i++) {  // copy elements 
+                arr_new[i] = arr[i];         // into new one
             }
-
-            // de-allocate the old array
-            delete[] arr;
-
-            // make lst.arr point to the new array
-            arr = arr_new;
+        
+            delete[] arr;   // de-allocate the old array
+            arr = arr_new;  // point to the new array
+            cout << "doubled capacity to " << capacity << "\n";
         }
-
-        // add x to the first unused location on the right end
         arr[size] = x;
         size++;
     }
 
-    int get_size() const { return size; }
-
-    // A setter: set(i, x) assigns a copy of x to location i of the
-    // underlying array of lst.
-    void set(int i, double x) {
-        if (i < 0 || i >= size) cmpt::error("set: index out of bounds");
-        arr[i] = x;
-    }
-
-    // A getter: returns the value at index location i of the
-    // underlying array.
-    double get(int i) const {
-        if (i < 0 || i >= size) cmpt::error("get: index out of bounds");
-        return arr[i];
-    }
-    
-    // Notice that get(i) is used in the for-loop, as a 
-    // demonstration for how to call it.
-    void print() const {
-        cout << "\n";
-        cout << "lst capacity = " << capacity << ", "
-             << "lst size = "     << size     << "\n";
-        for (int i = 0; i < size; i++) {
-            cout << "lst.arr[" << i << "] = " << get(i) << "\n";
-        }
-    }
-
+    // returns the sum of all elements in lst
     double sum() const {
         double result = 0;
-        for (int i = 0; i < size; i++) {
+        for(int i = 0; i < size; i++) {
             result += arr[i];
         }
         return result;
     }
 
+    // std::sort is C++'s standard sorting function from the <algorithm> include.
+    // The input to std::sort is a pointer to the first element of the underlying
+    // array, and a pointer to one past the last element.
     void sort_ascending() {
         std::sort(arr, arr + size);
     }
 
-    // Destructor. Always called automatically when the object is
-    // de-allocated. The programmer cannot call it manually.
-    ~double_list() {
-        delete[] arr;
+    void sort_descending() {
+        sort_ascending();
+        reverse();
     }
-}; // struct double_list
 
-double average(const double_list& lst) {
-    return lst.sum() / lst.get_size();
+    void reverse() {
+        std::reverse(arr, arr + size);
+    }
+}; // class double_list
+
+void outside_print(double_list lst) {
+    for (int i = 0; i < lst.get_size(); i++) {
+        cout << "   arr[" << i << "] = " << lst.get(i) << "\n";
+    }
+    cout << "capacity = " << lst.get_capacity() << ", "
+            << "size = "  << lst.get_size()     << "\n\n";
 }
 
-void sort_descending(double_list& lst) {
-    lst.sort_ascending();
-    // std::reverse(arr, arr + size);
-    int a = 0;
-    int b = lst.get_size() - 1;
-    while (a < b) {
-        double temp = lst.get(a); // temp = a
-        lst.set(a, lst.get(b));   // a = b
-        lst.set(b, temp);         // b = temp
-        a++;
-        b--;
+/*
+
+// set all elements of lst to fill_value
+void fill(double_list lst, int fill_value) {
+    for(int i = 0; i < lst.size; i++) {
+        lst.arr[i] = fill_value;
     }
 }
+
 
 int main() {
-    double_list lst(0);
+    double_list lst = make_empty_double_list();
+    
+    // add some numbers
+    append_right(lst, 6);
+    append_right(lst, -1);
+    append_right(lst, 2);
+    
+    // print their sum 
+    cout << "sum = " << sum(lst) << "\n";
+    
+    // sort from smallest to biggest
+    print(lst);
+    sort_ascending(lst);
+    print(lst);
 
-    // set the first three numbers of lst to be: 0, 6, 2.5
-    lst.append_right(0);
-    lst.append_right(6);
-    lst.append_right(2.6);
+    // fill with 5's
+    fill(lst, 5);
+    print(lst);
 
-    // print lst, one number per line
+    // add 100 more numbers
+    for(int i = 0; i < 100; i++) {
+        append_right(lst, i);
+    }
+    print(lst);
+
+    // de-allocate the underlying array to avoid a memory leak
+    deallocate(lst);
+
+    cout << "double_list.cpp: done\n";
+} // main
+*/
+
+int main() {
+    double_list lst;
     lst.print();
+    for(int i = 0; i < 100; i++) {
+        lst.append_right(i);
+    }
+    lst.print();
+    cout << "sum = " << lst.sum() << "\n";
 
-    // lst2 is a copy of lst
-    double_list lst2(lst);
-    lst2.print();
-
-    // print the sum and average
-    cout << "    sum = " << lst2.sum()    << "\n";
-    cout << "average = " << average(lst2) << "\n";
-
-    lst2.print();
+    double_list lst2;
+    lst2.append_right(5);
+    lst2.append_right(3);
+    lst2.append_right(7);
+    lst2.set(1, 4);
     lst2.sort_ascending();
     lst2.print();
-    sort_descending(lst2);
-    lst2.print();
 
-    // destructors automatically called
+    double_list lst3(lst2);
+    lst2.print();
+    outside_print(lst2);
 }
